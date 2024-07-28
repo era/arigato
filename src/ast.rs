04f8689fd05dfbd3033f1aba0fc28433405ff161
era <me@elias.sh>
2024-07-28 10:44:13 +0900

    parser

--- src/ast.rs
+++ src/ast.rs
@@ -4,3 +4,15 @@ pub enum Expr {
     Unary(Token, Box<Expr>),
     Binary(Box<Expr>, Token, Box<Expr>) 
 }
+
+///
+/// Name        Operators   Associates
+/// Equality    == !=       Left
+/// Comparison  > >= <= <   Left
+/// Term        - +         Left
+/// Factor      / *         Left
+/// Unary       ! -         Right
+pub struct Parser {
+    tokens: Vec<Token>,
+    curr: usize
+}
