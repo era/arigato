169479b68db977c9dfdddd6841d86352fab79f07
era <me@elias.sh>
2024-08-03 08:24:41 +0900

    parser

--- src/ast.rs
+++ src/ast.rs
@@ -45,11 +45,12 @@ impl Parser {
         self.tokens.next()
     }
 
+    // assignment
     fn expression(&mut self) -> Result<Box<Expr>> {
         self.equality()
     }
 
-    // term >= term; term >= term
+    // comparison ( ( "!=" | "==" ) comparison )*
     fn equality(&mut self) -> Result<Box<Expr>> {
         let mut comparison = self.comparison()?;
 
@@ -71,6 +72,7 @@ impl Parser {
         Ok(comparison)
     }
 
+    // term ( ( ">" | ">=" | "<" | "<=" ) term )*
     fn comparison(&mut self) -> Result<Box<Expr>> {
         let mut term = self.term()?;
         loop {
@@ -91,7 +93,49 @@ impl Parser {
         Ok(term)
     }
 
+    // factor ( ( "-" | "+" ) factor )*
     fn term(&mut self) -> Result<Box<Expr>> {
+        let mut factor = self.factor()?;
+        loop {
+            match self.peek() {
+                Some(&Token::Minus) | Some(&Token::Plus) => {
+                    let op = match self.advance() {
+                        Some(op) => op,
+                        None => return Ok(Box::new(Expr::EOF))
+                    };
+
+                    let right = self.factor()?;
+                    factor = Box::new(Expr::Binary(factor, op, right))
+                }
+                _ => break,
+            }
+        }
+
+        Ok(factor)
+    }
+
+    // unary ( ( "/" | "*" ) unary )* ;
+    fn factor(&mut self) -> Result<Box<Expr>> {
+        let mut unary = self.unary()?;
+        loop {
+            match self.peek() {
+                Some(&Token::Slash) | Some(&Token::Star) => {
+                    let op = match self.advance() {
+                        Some(op) => op,
+                        None => return Ok(Box::new(Expr::EOF))
+                    };
+
+                    let right = self.unary()?;
+                    unary = Box::new(Expr::Binary(unary, op, right))
+                }
+                _ => break,
+            }
+        }
+        Ok(unary)
+    }
+
+    // ( "!" | "-" ) unary | call ;
+    fn unary(&mut self) -> Result<Box<Expr>> {
         todo!()
     }
 }
