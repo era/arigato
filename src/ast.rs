82ebe7b15f6ae2c7011b4bb189eba863f7b3df31
era <me@elias.sh>
2024-08-03 08:05:00 +0900

    implementing parser

--- src/ast.rs
+++ src/ast.rs
@@ -4,10 +4,17 @@ use crate::lang::Token;
 use itertools::PeekNth;
 use itertools::peek_nth;
 
+pub enum ParserError {
+    Generic
+}
+
 pub enum Expr {
     Unary(Token, Box<Expr>),
-    Binary(Box<Expr>, Token, Box<Expr>) 
+    Binary(Box<Expr>, Token, Box<Expr>),
+    EOF
 }
+
+pub type Result<I> = std::result::Result<I, ParserError>;
 
 ///
 /// Name        Operators   Associates
@@ -20,7 +27,7 @@ pub struct Parser {
     tokens: PeekNth<std::vec::IntoIter<Token>>,
     curr: usize
 }
-
+// Recursive Descent Parsing
 impl Parser {
     fn new(tokens: Vec<Token>) -> Self {
         Self {
@@ -29,21 +36,62 @@ impl Parser {
         }
     }
 
+    fn peek(&mut self) -> Option<&Token> {
+        self.tokens.peek()
+    }
+
     fn advance(&mut self) -> Option<Token> {
         self.curr += 1;
         self.tokens.next()
     }
 
-    fn expr(&mut self) -> Expr {
-        todo!()
+    fn expression(&mut self) -> Result<Box<Expr>> {
+        self.equality()
     }
 
-    fn comparison(&mut self) -> Expr {
-        todo!()
+    // term >= term; term >= term
+    fn equality(&mut self) -> Result<Box<Expr>> {
+        let mut comparison = self.comparison()?;
+
+        loop {
+            match self.peek() {
+                Some(&Token::Equal) | Some(&Token::BangEqual) => {
+                    let op = match self.advance() {
+                        Some(op) => op,
+                        None => return Ok(Box::new(Expr::EOF))
+                    };
+
+                    let right = self.term()?;
+                    comparison = Box::new(Expr::Binary(comparison, op, right));
+                },
+                _ => break,
+            }
+        } 
+
+        Ok(comparison)
     }
 
-    // a == b; a != b
-    fn equality(&mut self) -> Expr {
+    fn comparison(&mut self) -> Result<Box<Expr>> {
+        let mut term = self.term()?;
+        loop {
+            match self.peek() {
+                Some(&Token::Greater) | Some(&Token::GreaterEqual) | Some(Token::Less) | Some(Token::LessEqual) => {
+                    let op = match self.advance() {
+                        Some(op) => op,
+                        None => return Ok(Box::new(Expr::EOF))
+                    };
+
+                    let right = self.term()?;
+                    term = Box::new(Expr::Binary(term, op, right))
+                }
+                _ => break,
+            }
+        }
+
+        Ok(term)
+    }
+
+    fn term(&mut self) -> Result<Box<Expr>> {
         todo!()
     }
 }
