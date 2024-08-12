81e3b68d68fb114d9336abf3a23fec680facc39c
era <me@elias.sh>
2024-08-12 10:33:23 +0900

    simplify design of interpreter

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -14,15 +14,24 @@ pub struct Interpreter {}
 
 // a tree-walking interpreter implemented by using the visitor pattern.
 impl Interpreter {
-    fn visitLiteralExpr(lit: Expr) -> Result<Type, Error> {
-        let token = match lit {
-            Expr::Literal(token) => token,
-            _ => return Err(Error::UnexpectedExpr("expecting only literals")),
-        };
+
+    fn evaluate(mut self, expr: Expr) -> Result<Type, Error> {
+        match expr {
+            Expr::Literal(token) => self.literal_expr(token),
+            Expr::Grouping(expr) => self.grouping_expr(*expr),
+            _ => todo!(),
+        }
+    }
+
+    fn literal_expr(mut self, token: Token) -> Result<Type, Error> {
         match token {
             Token::Number(n) => Ok(Type::Number(n)),
             Token::Text(t) => Ok(Type::Text(t)),
             _ => Err(Error::UnexpectedExpr("expecting only literals"))
         }
+    }
+
+    fn grouping_expr(mut self, g: Expr) -> Result<Type, Error> {
+         return self.evaluate(g)
     }
 }
