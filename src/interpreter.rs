c576497a9844340248f94ab7601006b70db2e128
era <me@elias.sh>
2024-08-12 10:35:48 +0900

    avoid consuming itself

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -15,7 +15,7 @@ pub struct Interpreter {}
 // a tree-walking interpreter implemented by using the visitor pattern.
 impl Interpreter {
 
-    fn evaluate(mut self, expr: Expr) -> Result<Type, Error> {
+    fn evaluate(&mut self, expr: Expr) -> Result<Type, Error> {
         match expr {
             Expr::Literal(token) => self.literal_expr(token),
             Expr::Grouping(expr) => self.grouping_expr(*expr),
@@ -23,7 +23,7 @@ impl Interpreter {
         }
     }
 
-    fn literal_expr(mut self, token: Token) -> Result<Type, Error> {
+    fn literal_expr(&mut self, token: Token) -> Result<Type, Error> {
         match token {
             Token::Number(n) => Ok(Type::Number(n)),
             Token::Text(t) => Ok(Type::Text(t)),
@@ -31,7 +31,7 @@ impl Interpreter {
         }
     }
 
-    fn grouping_expr(mut self, g: Expr) -> Result<Type, Error> {
+    fn grouping_expr(&mut self, g: Expr) -> Result<Type, Error> {
          return self.evaluate(g)
     }
 }
